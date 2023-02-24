3defe85648410ddcabadef4c209e0bf61d5bb022
shinandmin <shinandmin13@naver.com>
2023-02-24 13:44:24 +0900

    Revert "20230224"
    This reverts commit e9d0f82343cad7aa4d25e2688fa9602922c06e67.

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -8,12 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    @auth
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
-                    @endauth
-                    @guest
-                        <a class="nav-link active" aria-current="page" href="/auth/signin">Home</a>
-                    @endguest
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
                     @auth
