8d32000f161e389b2cb151cdb2c700922fd0551e
shinandmin <shinandmin13@naver.com>
2023-02-24 13:43:50 +0900

    Revert "20230224"
    This reverts commit e5a593ba

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,24 +3,7 @@
 @section('head_include')
 
 @section('content')
-    <div class="wrap col-lg-12">
-        <div class="container col-lg-3 col-md-6 col-sm-12 mt-5 p-4 border">
-            <form method="POST" action="{{ url('/auth/signin') }}">
-                @csrf
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Userid</label>
-                    <input type="text" class="form-control" name="userid" id="userid">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" id="password">
-                </div>
-                <div class="d-grid gap-2 d-md-flex justify-content-sm-end">
-                    <a href="{{ url('/signup') }}"><button class="btn btn-primary me-md-2" type="button">회원가입</button></a>
-                    <button class="btn btn-primary" type="submit">로그인</button>
-                </div>
-
-            </form>
-        </div>
-    </div>
+<div class="wrap">
+    hi
+</div>
 @endsection
